d4276fdfb6ddaac1eaa428c583971bb402d967a7
flsdqm <592661681@qq.com>
2021-12-11 09:55:48 +0800

    安卓编程权威指南书上代码

---
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/Question.java
@@ -0,0 +1,37 @@
+package com.example.geoquiz;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+    //private boolean mIsAnswer;
+
+    public Question(int textResId,boolean answerTrue){
+        mTextResId=textResId;
+        mAnswerTrue=answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    /*public boolean isAnswer() {
+        return mIsAnswer;
+    }
+
+    public void setAnswer(boolean answer) {
+        mIsAnswer = answer;
+    }
+*/
+}
